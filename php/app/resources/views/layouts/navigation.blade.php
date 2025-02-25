147e486d768863793643a70ab566982db578d509
yuma346west <ub.yuma.cloe@gmail.com>
2025-02-26 02:44:53 +0900

    なんとなく履歴書作成サービスを作り始めた

--- php/app/resources/views/layouts/navigation.blade.php
+++ php/app/resources/views/layouts/navigation.blade.php
@@ -16,6 +16,14 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                        {{ __('Dashboard') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('resume')" :active="request()->routeIs('resume')">
+                        {{ __('履歴書') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
--- php/app/routes/web.php
+++ php/app/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ResumeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,6 +16,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // routes/web.php に追加
+    Route::get('/resume', [ResumeController::class, 'index'])->name('resume');
 });
 Route::get('/health', function () {
     return response()->json(['status' => 'OK'], 200);
